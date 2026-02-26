59958b6c836fcb605fc055bf919c0febc1d08971
Hafeez <syedhafeez957@gmail.com>
2026-02-26 15:07:32 +0530

    fetched api using axios

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
 const Products = () => {
+  let fetchApi=async ()=>{
+    let data=await axios.get("http://localhost:4000/products")
+    console.log(data);
+    
+  }
+
+  useEffect(()=>{
+    fetchApi()
+  },[])
+  
   return (
     < >
       <h1>Products</h1>
