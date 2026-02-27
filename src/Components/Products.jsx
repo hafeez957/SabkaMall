461624d489f425b77be19823eb17301cca7f649b
Hafeez <syedhafeez957@gmail.com>
2026-02-27 23:47:10 +0530

    displaying products category wise

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,20 +1,70 @@
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import '../assets/styles/products.css'
+import { Link, NavLink } from 'react-router-dom'
 
 const Products = () => {
-  let fetchApi=async ()=>{
-    let data=await axios.get("http://localhost:4000/products")
-    console.log(data);
-    
+  let [products, setProducts] = useState([])
+  let [filtered_data, setFiltered_data] = useState([])
+  let fetchApi = async () => {
+    let data = await axios.get("http://localhost:4000/products")
+    setProducts(data.data);
+    setFiltered_data(data.data)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchApi()
   },[])
-  
+
+  let handleCategory=(e)=>{
+    let temp=(e.target.innerText).toLowerCase()
+    let filtered_products=products.filter((ele)=>{
+      if(temp==="all"){
+        return products
+      }
+      return ele.category===temp
+    })
+    setFiltered_data(filtered_products)
+  }
+
+
+  // console.log(filtered_products);
   return (
     < >
+
+      
+      <div className="products">
       <h1>Products</h1>
+
+        <div className="category">
+          <ul>
+            <li><button onClick={handleCategory}>All</button></li>
+            <li><button onClick={handleCategory}>Men's clothing</button></li>
+            <li><button onClick={handleCategory}>Women's clothing</button></li>
+            <li><button onClick={handleCategory}>Jewelery</button></li>
+            <li><button onClick={handleCategory}>Electronics</button></li>
+          </ul>
+        </div>
+        {
+        
+        
+        filtered_data.map((ele) => {
+        let {title,price,description,image}=ele;
+        return(
+          <div className="card">
+            <h4>{title}</h4>
+            <img src={image} alt="" />
+            {/* <p>{description}</p> */}
+             <button>${price}</button> 
+            <div className="info">
+              <button>View More</button>
+            <button>Delete</button>
+            </div>
+          </div>
+        )
+      })
+      }
+      </div>
     </ >
   )
 }
