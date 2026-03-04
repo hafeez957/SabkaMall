23494e93ec331553b1dc53ba1ff18dd995050c46
Hafeez <syedhafeez957@gmail.com>
2026-03-04 16:05:09 +0530

    Updated User portal

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import '../assets/styles/navbar.css'
 import SearchIcon from '@mui/icons-material/Search';
 import logo from '../assets/images/logo.png'
 
 const Navbar = () => {
+  let location=useLocation()
+  let bool=location.pathname.startsWith('/adminportal');
+  
   return (
     < >
       <div className="navbar">
@@ -16,14 +19,20 @@ const Navbar = () => {
             <button className='icon'><SearchIcon/></button>
         </div>
         <div className="links">
-            <ul>
+            {bool?<ul>
                 <li><NavLink className={'link'} to={'/adminportal/'}>Home</NavLink></li>
                 <li><NavLink className={'link'} to={'/adminportal/products'}>Products</NavLink></li>
                 <li><NavLink className={'link'} to={'/adminportal/addproduct'}>Add products</NavLink></li>
                 <li><NavLink className={'link'} to={'/adminportal/users'}>Users</NavLink></li>
                 <li><NavLink className={'link'} to={'/adminportal/addusers'}>Add Users</NavLink></li>
                 <li><NavLink>Logout</NavLink></li>
-            </ul>
+            </ul>:<ul>
+                <li><NavLink className={'link'} to={'/userportal'}>Home</NavLink></li>
+                <li><NavLink className={'link'} to={'/userportal/products'}>Products</NavLink></li>
+                <li><NavLink className={'link'} to={'/userportal/about '}>About us</NavLink></li>
+                <li><NavLink className={'link'} to={'/userportal/help '}>Help</NavLink></li>
+                <li><NavLink>Logout</NavLink></li>
+            </ul>}
         </div>
       </div>
     </ >
--- src/Components/User/UserPortal.jsx
+++ src/Components/User/UserPortal.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import UserHome from './Cart'
+// import UserHome from './Cart'
 import Products from '../Products'
+import ViewMore from '../ViewMore'
+import Users from '../Admin/Users'
+import Navbar from '../Navbar'
+import About from './About'
+import Help from './Help'
 
 const UserPortal = () => {
   return (
     < >
+    <Navbar/>
       <Routes>
-        <Route element={<Products/>} path='/' />
+        <Route element={<Products/>} path='/products' />
+        <Route element={<ViewMore/>} path='/viewmore/:id' />
+        <Route element={<Users/>} path='/users' />
+        <Route element={<About/>} path='/about' />
+        <Route element={<Help/>} path='/help' />
       </Routes>
     </ >
   )
